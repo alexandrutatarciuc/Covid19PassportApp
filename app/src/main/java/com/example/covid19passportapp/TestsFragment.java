c1e19821a90ec2568460bf47349e86987fd9f698
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-04-28 18:15:52 +0200

    floating action button

--- app/src/main/java/com/example/covid19passportapp/TestsFragment.java
+++ app/src/main/java/com/example/covid19passportapp/TestsFragment.java
@@ -3,10 +3,13 @@ package com.example.covid19passportapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,33 +18,16 @@ import android.view.ViewGroup;
  */
 public class TestsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FloatingActionButton actionButton;
 
     public TestsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TestsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TestsFragment newInstance(String param1, String param2) {
+
+    public static TestsFragment newInstance() {
         TestsFragment fragment = new TestsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +35,20 @@ public class TestsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tests, container, false);
+        View view = inflater.inflate(R.layout.fragment_tests, container, false);
+
+        actionButton = view.findViewById(R.id.testsFab);
+        actionButton.setOnClickListener((v) -> NavHostFragment.findNavController(this).navigate(R.id.openAddTestFragmentAction));
+
+
+
+        return view;
     }
 }
