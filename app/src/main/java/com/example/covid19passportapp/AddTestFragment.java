ab0b2b539fd1d2d2d727d4f824585f7ac5993767
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-04-30 10:37:34 +0200

    fixing date picker + styling

--- app/src/main/java/com/example/covid19passportapp/AddTestFragment.java
+++ app/src/main/java/com/example/covid19passportapp/AddTestFragment.java
@@ -4,61 +4,79 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddTestFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.joda.time.DateTime;
+
+import java.util.Date;
+
 public class AddTestFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MaterialDatePicker datePicker;
+    private TextInputEditText dateInput;
+    private DateTime selectedDate;
 
     public AddTestFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddTestFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddTestFragment newInstance(String param1, String param2) {
-        AddTestFragment fragment = new AddTestFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_test, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_test, container, false);
+
+        datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Testing Date")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build();
+
+        datePicker.addOnCancelListener((dialogInterface) -> {
+            dateInput.clearFocus();
+        });
+
+        datePicker.addOnDismissListener((dialogInterface) -> {
+            dateInput.clearFocus();
+        });
+
+        datePicker.addOnPositiveButtonClickListener((selection) -> {
+            selectedDate = new DateTime(selection);
+            dateInput.setText(selectedDate.toString("dd/MM/yyyy"));
+        });
+
+        dateInput = view.findViewById(R.id.testDateEditInput);
+
+        dateInput.setOnFocusChangeListener((v1, s) -> {
+            if (v1.getId() == R.id.testDateEditInput && s) {
+                datePicker.show(this.getParentFragmentManager(), "DATE_PICKER");
+            }
+        });
+
+        dateInput.setOnClickListener((v) -> {
+            datePicker.show(this.getParentFragmentManager(), "DATE_PICKER");
+        });
+
+
+        return view;
+
+
     }
+
+
 }
--- app/src/main/java/com/example/covid19passportapp/Models/TestsRecyclerAdapter.java
+++ app/src/main/java/com/example/covid19passportapp/Models/TestsRecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.covid19passportapp.Models;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,8 +33,18 @@ public class TestsRecyclerAdapter extends RecyclerView.Adapter<TestsRecyclerAdap
     public void onBindViewHolder(@NonNull TestsRecyclerAdapter.ViewHolder holder, int position) {
         Test test = tests.get(position);
         holder.id.setText(String.valueOf(test.getID()));
+        holder.result.setTextColor(Color.RED);
         holder.date.setText(test.getDate().toString("dd/MM/yy"));
         holder.result.setText(test.getResult());
+        if (test.getResult().equalsIgnoreCase("positive")) {
+            holder.result.setTextColor(Color.parseColor("#FF0023"));
+        }
+        else if (test.getResult().equalsIgnoreCase("negative")) {
+            holder.result.setTextColor(Color.parseColor("#64B31D"));
+        }
+        else {
+            holder.result.setTextColor(Color.BLACK);
+        }
     }
 
     @Override
