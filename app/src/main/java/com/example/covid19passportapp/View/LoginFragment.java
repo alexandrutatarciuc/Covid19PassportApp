59848438331dab5c6923bb59e4dcc02f63f66ea2
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-05-02 15:18:46 +0200

    Update LoginFragment.java

--- app/src/main/java/com/example/covid19passportapp/View/LoginFragment.java
+++ app/src/main/java/com/example/covid19passportapp/View/LoginFragment.java
@@ -3,14 +3,29 @@ package com.example.covid19passportapp.View;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.covid19passportapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginFragment extends Fragment {
+
+    private TextInputEditText emailInput;
+    private TextInputEditText passwordInput;
+    private Button loginButton;
+    private Button registerButtonForward;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -26,9 +41,49 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        emailInput = view.findViewById(R.id.loginEmailEditInput);
+        passwordInput = view.findViewById(R.id.loginPasswordEditInput);
+        loginButton = view.findViewById(R.id.loginButton);
+        registerButtonForward = view.findViewById(R.id.registerButtonForward);
+
+        loginButton.setOnClickListener(v -> login());
+        registerButtonForward.setOnClickListener(v -> registerForward());
+
+
+        //Validating input
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean isFilledEmail = !emailInput.getText().toString().isEmpty();
+                boolean isFilledPassword = !passwordInput.getText().toString().isEmpty();
+                boolean setEnabled = isFilledEmail && isFilledPassword;
+                loginButton.setEnabled(setEnabled);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //nothing
+            }
+        };
+
+        emailInput.addTextChangedListener(textWatcher);
+        passwordInput.addTextChangedListener(textWatcher);
 
         return view;
+    }
+
+    private void login() {
+        ((MainActivity)getActivity()).goToMain();
+    }
+
+    private void registerForward() {
+        NavHostFragment.findNavController(this).navigate(R.id.openRegisterFragmentAction);
     }
 }
