10894d16f79f49faeb2511cb95a18732f88512f2
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-05-02 16:00:18 +0200

    register user

--- app/src/main/java/com/example/covid19passportapp/View/RegisterFragment.java
+++ app/src/main/java/com/example/covid19passportapp/View/RegisterFragment.java
@@ -3,15 +3,27 @@ package com.example.covid19passportapp.View;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.covid19passportapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class RegisterFragment extends Fragment {
+
+    private TextInputEditText emailInput;
+    private TextInputEditText passwordInput;
+    private Button registerButton;
+    private Button backToLoginButton;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -29,7 +41,38 @@ public class RegisterFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_register, container, false);
 
+        emailInput = view.findViewById(R.id.registerEmailEditInput);
+        passwordInput = view.findViewById(R.id.registerPasswordEditInput);
+        registerButton = view.findViewById(R.id.registerButton);
+        backToLoginButton = view.findViewById(R.id.goBackToLoginButton);
+
+        registerButton.setOnClickListener(v -> register());
+        backToLoginButton.setOnClickListener(v -> backToLogin());
 
         return view;
+    }
+
+    private void register() {
+        Log.d("REGISTER", "CLICKED");
+
+        try {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailInput.getText().toString(), passwordInput.getText().toString()).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getContext(), "User successfully registered", Toast.LENGTH_SHORT).show();
+                    backToLogin();
+                } else {
+                    Log.d("REGISTER", task.getException().getMessage());
+                    Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void backToLogin() {
+        NavHostFragment.findNavController(this).popBackStack();
+        NavHostFragment.findNavController(this).popBackStack();
+        NavHostFragment.findNavController(this).navigate(R.id.loginFragment);
     }
 }
