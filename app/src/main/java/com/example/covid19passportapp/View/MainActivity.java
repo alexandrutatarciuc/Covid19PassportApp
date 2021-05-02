aa01e6e7d81d39a7619e196b7cce0ee6467b6032
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-05-02 15:04:55 +0200

    separate navigation for login

--- app/src/main/java/com/example/covid19passportapp/View/MainActivity.java
+++ app/src/main/java/com/example/covid19passportapp/View/MainActivity.java
@@ -12,11 +12,16 @@ import androidx.navigation.ui.NavigationUI;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.covid19passportapp.R;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     private NavController navController;
     private DrawerLayout drawerLayout;
@@ -26,13 +31,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_main);
+
+        /*NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(drawerLayout).build();
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        //Allows navigation to destinations when clicking menu items in the navigation drawer
+        NavigationUI.setupWithNavController(navigationView, navController);*/
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        /*FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            goToMain();
+        } else {
+            goToLogin();
+        }*/
+    }
+
+    public void goToMain() {
+        //TODO
         setContentView(R.layout.activity_main);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         navController = navHostFragment.getNavController();
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.nav_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        /*setSupportActionBar(toolbar);*/
+        //setSupportActionBar(toolbar);
 
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(drawerLayout).build();
 
@@ -42,8 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        return navController.navigateUp() || super.onSupportNavigateUp();
+    private void goToLogin() {
+        //TODO
     }
 }
