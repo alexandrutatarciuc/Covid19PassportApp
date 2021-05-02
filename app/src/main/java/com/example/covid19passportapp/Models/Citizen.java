e8cf1eb472301fcb765b7fef42aea7e486424aec
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2021-05-02 17:58:10 +0200

    citizen model

---
+++ app/src/main/java/com/example/covid19passportapp/Models/Citizen.java
@@ -0,0 +1,55 @@
+package com.example.covid19passportapp.Models;
+
+import org.joda.time.DateTime;
+
+import java.util.List;
+
+public class Citizen {
+
+    private String fullName;
+    private DateTime birthdate;
+    private Passport passport;
+    private List<Test> tests;
+
+    public Citizen() {
+    }
+
+    public Citizen(String fullName, DateTime birthdate, Passport passport, List<Test> tests) {
+        this.fullName = fullName;
+        this.birthdate = birthdate;
+        this.passport = passport;
+        this.tests = tests;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public DateTime getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(DateTime birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
+
+    public List<Test> getTests() {
+        return tests;
+    }
+
+    public void setTests(List<Test> tests) {
+        this.tests = tests;
+    }
+}
